6a7dee4ca12b21b3b813c458db4bc2315d829d86
ArthurStempniak <arthurstempniak8@gmail.com>
2026-03-30 14:56:08 -0300

    Update calculadora.js
    Tinha esquecido de formatar corretamente.

--- Primeiro_Bimestre/Atv5/calculadora.js
+++ Primeiro_Bimestre/Atv5/calculadora.js
@@ -7,5 +7,6 @@ gorjeta = valorConta * (percentualGorjeta / 100);
 
 valorTotal = gorjeta + valorConta;
 
-console.log("Valor da Conta: " + valorTotal);
-console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": "+ gorjeta);
+console.log("Valor da Conta: R$" + valorConta.toFixed(2));
+console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": R$"+ gorjeta.toFixed(2));
+console.log("Valor Total a Pagar: R$"+valorTotal.toFixed(2));
