04ddf36f6d3d0819a11094829c37a7cc13a9fe39
ArthurStempniak <arthurstempniak8@gmail.com>
2026-03-30 14:37:06 -0300

    Create calculadora.js

---
+++ Primeiro_Bimestre/Atv5/calculadora.js
@@ -0,0 +1,11 @@
+valorConta = 100;
+percentualGorjeta = 15;
+
+gorjeta = valorConta * (percentualGorjeta / 100);
+
+
+
+valorTotal = gorjeta + valorConta;
+
+console.log("Valor da Conta: " + valorTotal);
+console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": "+ gorjeta);
