7bdfcffefca6b496ed8d389c7875846156301d9b
ArthurStempniak <arthurstempniak8@gmail.com>
2026-03-30 15:01:28 -0300

    Atualização 2.0 Calculadora
    Aproveitei pra tentar entender como era feito no JS em questão de input no terminal, como faziamos no Java.

--- Primeiro_Bimestre/Atv5/calculadora.js
+++ Primeiro_Bimestre/Atv5/calculadora.js
@@ -3,10 +3,32 @@ percentualGorjeta = 15;
 
 gorjeta = valorConta * (percentualGorjeta / 100);
 
-
-
 valorTotal = gorjeta + valorConta;
 
-console.log("Valor da Conta: R$" + valorConta.toFixed(2));
-console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": R$"+ gorjeta.toFixed(2));
-console.log("Valor Total a Pagar: R$"+valorTotal.toFixed(2));
+    console.log("Valor da Conta: R$" + valorConta.toFixed(2));
+    console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": R$"+ gorjeta.toFixed(2));
+    console.log("Valor da total a Pagar: R$" + valorTotal.toFixed(2) + "\n");
+
+const input = require('prompt-sync')();
+
+const entrada = input("Deseja alterar o valor da gorjeta? 1) Sim | 2) Não ---> ");
+
+if(entrada == 1){
+    const novovalorgorjeta = Number(input("Informe o valor em decimal. (ex: 10, 20..) ---> "));
+    percentualGorjeta = novovalorgorjeta;
+
+    const novovalorconta = Number(input("Informe o valor da nova conta ---> "));
+    valorConta = novovalorconta;
+
+    
+    gorjeta = valorConta * (percentualGorjeta / 100);
+    valorTotal = valorConta + gorjeta;
+
+    console.log("\n");
+
+    console.log("Valor da Conta: R$" + valorConta.toFixed(2));
+    console.log("Valor da Gorjeta "+ "(" + percentualGorjeta + "%" + ")" +": R$"+ gorjeta.toFixed(2));
+    console.log("Valor da total a Pagar: R$" + valorTotal.toFixed(2) + "\n");
+}else{
+    console.log("Tu fez coisa errada");
+}
